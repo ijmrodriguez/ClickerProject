8325e9502d5a1ab4c6d16d04a1ab82792c6f52ae
ijmrodriguez <ijonmrodriguez@gmail.com>
2024-01-29 12:56:24 -0800

    modified MainActivity

--- app/src/main/java/com/example/clickerproject/MainActivity.java
+++ app/src/main/java/com/example/clickerproject/MainActivity.java
@@ -1,23 +1,28 @@
 package com.example.clickerproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
     String[] dogs = {"dog1", "dog2", "dog3", "dogs4", "dog5"};
+    ImageView dogImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dogImage = findViewById(R.id.dogImage);
     }
 
-    public void setDogs()
+    public void setDogs(View view)
     {
-
+        for (String dog : dogs) {
+            int resourceId = getResources().getIdentifier(dog, "drawable", getPackageName());
+            dogImage.setImageResource(resourceId);
+        }
     }
-
 }
