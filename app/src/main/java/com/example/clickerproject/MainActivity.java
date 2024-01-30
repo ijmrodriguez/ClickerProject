b385a312da5e856b5c1b391ea9317039b9e3d807
ijmrodriguez <ijonmrodriguez@gmail.com>
2024-01-30 12:46:48 -0800

    Updated errors with the counter text view. Finished project.

--- app/src/main/java/com/example/clickerproject/MainActivity.java
+++ app/src/main/java/com/example/clickerproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.clickerproject;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity
     int currentImageIndex = 0;
     int[] dogImages = {R.drawable.dog1, R.drawable.dog2, R.drawable.dog3,
             R.drawable.dog4, R.drawable.dog5};
+    int pressCount = 0;
+
+    TextView counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,11 +23,14 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dogImage = findViewById(R.id.dogImage);
+        counter = findViewById(R.id.counterTextView);
     }
 
     public void setDogs(View view)
     {
       currentImageIndex = (currentImageIndex + 1) % dogImages.length;
       dogImage.setImageResource(dogImages[currentImageIndex]);
+      pressCount++;
+      counter.setText("Press Count: " + pressCount);
     }
 }
